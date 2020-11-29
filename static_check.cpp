19e4632e42af8e7fe9eaf8e4a15e6eb8d64b856f
Dzu Nguyen <dzutnguyen@gmail.com>
2020-11-29 16:18:26 -0500

    safe static check

--- static_check.cpp
+++ static_check.cpp
@@ -12,15 +12,30 @@ struct CompileTimeChecker<false> {}; // default ctor doesn't take any argument
 #define STATIC_CHECK(expr, mssg) \
 {   \
     class ERROR_##mssg{ }; \
-    (void)sizeof(CompileTimeChecker<expr>(ERROR_##mssg())); \
+    (void)sizeof( (CompileTimeChecker<expr>(ERROR_##mssg())) ); \
+}
+
+template<typename To, typename From>
+To safe_reinterpret_cast(From a)
+{
+    STATIC_CHECK (sizeof(To) >= sizeof(From), NARROW_CAST );
+    return reinterpret_cast<To>(a);
 }
 
 
-
-
-void main()
+int main()
 {
-    // STATIC_CHECK(sizeof(int) == sizeof(int), VOID_PTR_NOT_EQUAL_CHAR);
+    STATIC_CHECK(sizeof(void*) == sizeof(long), VOID_PTR_NOT_EQUAL_CHAR) // no compile error for 64-bit system
+    
+    // char a = safe_reinterpret_cast<int>(23);
+    // auto b = safe_reinterpret_cast<char>(8);
+    int a = 2;
+    auto c = safe_reinterpret_cast<double*>(&a);
+
+    double b = 1.0;
+    auto d = safe_reinterpret_cast<int*>(&b);
+
+    return 0;
 }
 
 
